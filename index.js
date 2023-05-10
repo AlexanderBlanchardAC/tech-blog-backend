19638db40bd355dcab41b4c1509c8e85c764f563
AlexanderBlanchardAC <124055686+AlexanderBlanchardAC@users.noreply.github.com>
2023-05-10 14:40:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const secret = 'asdghtredvbjmkkhyrr5689hggbnhgfd7';
 
 
 app.use(cors({
-    origin:'https://techblog-api-0wpd.onrender.com',
+    origin:'https://techblog-api-m083.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
     })
